189d1e890ea5ca9d3c41af4f076a93d17aec2dd9
Yuze Wang <yuzewang621@gmail.com>
2023-07-09 15:29:33 -0600

    add schema to initial database

---
+++ backend/db/schema/01_create_users.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  profile_url VARCHAR(255),
+  descprition TEXT,
+  password_hash VARCHAR(255) NOT NULL,
+  last_modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
---
+++ backend/db/schema/02_create_reviews.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  movie_id INTEGER NOT NULL,
+  rating INTEGER,
+  review TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,  
+  UNIQUE (user_id, movie_id)
+);
