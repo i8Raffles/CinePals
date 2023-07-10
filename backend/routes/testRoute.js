fde56e8c01091443fdde31c5b24205d993b06c3d
Yuze Wang <yuzewang621@gmail.com>
2023-07-10 02:30:53 -0600

    delete routes/testRoute, reframe to src/routes

--- backend/routes/testRoute.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-router.get('/', (req, res) => {
-  const movies = ['Star Wars', 'Spiderman', 'Warcraft', 'James Bond'];
-  res.json(movies);
-});
-  
-
-module.exports = router;
