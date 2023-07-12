7b4bc76524971ca9afcf9c3d0511442ea9fd3ab3
nifen44 <huangwei19810803@gmail.com>
2023-07-12 11:42:19 -0600

    hook up back end API & more UI

--- frontend/src/components/MovieThumbnail.jsx
+++ frontend/src/components/MovieThumbnail.jsx
@@ -16,10 +16,10 @@ import { Link } from "react-router-dom";
 
 function MovieThumbnail(props) {
     return <Card sx={{
-        minWidth: 120, maxWidth: 240,
+        minWidth: 180, maxWidth: 270,
         overflow: 'hidden',
         flexGrow: 1, flexShrink: 0,
-        width: '15%', flexBasis: 'auto' }}>
+        width: '20%', flexBasis: 'auto' }}>
         <CardHeader sx={{ whiteSpace: 'nowrap' }} avatar={
             <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">{props.movie.name[0]}</Avatar>
         } title={props.movie.name} subheader={props.movie.releaseDate}/>
