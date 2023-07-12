d7d7336c45a9902b22bea572ec3e1888b06f088b
Md Muhtasim Yasser <muhtasimyasser@gmail.com>
2023-07-11 23:18:14 -0400

    update review,users queries

--- backend/src/routes/reviews.js
+++ backend/src/routes/reviews.js
@@ -1,18 +1,72 @@
 const router = require("express").Router();
 module.exports = db => {
-  router.get("/reviews", (request, response) => {
-    db.query(`
-      SELECT 
-      reviews.id,
-      reviews.movie_id,
-      reviews.rating,
-      reviews.review,
-      reviews.created_at,
-      reviews.user_id
-      FROM reviews
-    `).then(({ rows:reviews }) => {
-      response.json(reviews);
-    });
+  // router.get("/reviews", (request, response) => {
+  //   db.query(`
+  //     SELECT 
+  //     reviews.id,
+  //     reviews.movie_id,
+  //     reviews.rating,
+  //     reviews.review,
+  //     reviews.created_at,
+  //     reviews.user_id
+  //     FROM reviews
+  //   `).then(({ rows:reviews }) => {
+  //     response.json(reviews);
+  //   });
+  // });
+
+  // GET all reviews
+  router.get("/reviews", async (request, response) => {
+    try {
+      const reviews = await db.query("SELECT * FROM reviews;");
+      response.json(reviews.rows);
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error: "An error occurred." });
+    }
+  });
+  
+  // GET all reviews by userId
+  router.get("/reviews/user/:userId", async (request, response) => {
+    const { userId } = request.params;
+    try {
+      const reviews = await db.query("SELECT * FROM reviews WHERE user_id = $1;", [userId]);
+      response.json(reviews.rows);
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error: "An error occurred." });
+    }
+  });
+
+  // GET review by id
+  router.get("/reviews/:id", async (request, response) => {
+    const { id } = request.params;
+    try {
+      const review = await db.query("SELECT * FROM reviews WHERE id = $1;", [id]);
+      if (review.rows.length === 0) {
+        response.status(404).json({ error: "Review not found." });
+      } else {
+        response.json(review.rows[0]);
+      }
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error: "An error occurred." });
+    }
+  });
+
+  // POST new review
+  router.post("/reviews", async (request, response) => {
+    const { movieId, rating, review, userId } = request.body;
+    try {
+      const createdReview = await db.query(
+        "INSERT INTO reviews (movie_id, rating, review, user_id) VALUES ($1, $2, $3, $4) RETURNING *;",
+        [movieId, rating, review, userId]
+      );
+      response.status(201).json(createdReview.rows[0]);
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error: "An error occurred." });
+    }
   });
 
   return router;
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,21 +1,92 @@
 const router = require("express").Router();
 module.exports = db => {
-  router.get("/users", (request, response) => {
-    db.query(`
-      SELECT 
-      users.id,
-      users.first_name,
-      users.last_name,
-      users.email,
-      users.profile_url,
-      users.profile_description,
-      users.password_hash,
-      users.last_modified_at
-      FROM users
-    `).then(({ rows:users }) => {
-      response.json(users);
-    });
+  // router.get("/users", (request, response) => {
+  //   db.query(`
+  //     SELECT 
+  //     users.id,
+  //     users.first_name,
+  //     users.last_name,
+  //     users.email,
+  //     users.profile_url,
+  //     users.profile_description,
+  //     users.password_hash,
+  //     users.last_modified_at
+  //     FROM users
+  //   `).then(({ rows:users }) => {
+  //     response.json(users);
+  //   });
+  // });
+
+  // GET all users
+  router.get("/users", async (request, response) => {
+    try {
+      const users = await db.query("SELECT id, first_name, last_name, email, profile_url, profile_description, password_hash, last_modified_at FROM users;");
+      response.json(users.rows);
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error: "An error occurred." });
+    }
   });
+  
+  // GET user by id
+  router.get("/users/:id", async (request, response) => {
+    const { id } = request.params;
+    try {
+      const user = await db.query("SELECT * FROM users WHERE id = $1;", [id]);
+      if (user.rows.length === 0) {
+        response.status(404).json({ error: "User not found." });
+      } else {
+        response.json(user.rows[0]);
+      }
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error: "An error occurred." });
+    }
+  });
+
+  // POST new user into database
+  router.post("/users", async (request, response) => {
+    const {
+      firstName,
+      lastName,
+      username,
+      email,
+      profileUrl,
+      profileDescription,
+      passwordHash
+    } = request.body;
+    try {
+      const createdUser = await db.query(
+        "INSERT INTO users (first_name, last_name, username, email, profile_url, profile_description, password_hash) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;",
+        [firstName, lastName, username, email, profileUrl, profileDescription, passwordHash]
+      );
+      response.status(201).json(createdUser.rows[0]);
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error: "An error occurred." });
+    }
+  });
+
+  // UPDATE existing user details(profile picture url and profile description)
+  router.put("/users/:id", async (request, response) => {
+    const { id } = request.params;
+    const { profileUrl, profileDescription } = request.body;
+    try {
+      const updatedUser = await db.query(
+        "UPDATE users SET profile_url = $1, profile_description = $2 WHERE id = $3 RETURNING *;",
+        [profileUrl, profileDescription, id]
+      );
+      if (updatedUser.rows.length === 0) {
+        response.status(404).json({ error: "User not found." });
+      } else {
+        response.json(updatedUser.rows[0]);
+      }
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error: "An error occurred." });
+    }
+  });
+
 
   return router;
 };
