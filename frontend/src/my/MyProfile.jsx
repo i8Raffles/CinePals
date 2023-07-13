589570ec26ea2912835e48da48dede40e5eecccd
Yuze Wang <yuzewang621@gmail.com>
2023-07-13 00:09:42 -0600

    move usedata part to useMyProfile

--- frontend/src/my/MyProfile.jsx
+++ frontend/src/my/MyProfile.jsx
@@ -1,28 +1,9 @@
 import { Box, Button, TextField, Typography, Avatar } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import useMyProfile from "./useMyProfile";
 
 function MyProfile() {
-  const [avatarUrl, setAvatarUrl] = useState("");
-  const [description, setDescription] = useState("");
-  const [user, setUser] = useState(null);
-  const userId = 1;
-
-  useEffect(() => {
-    axios
-      .get(`/api/users/${userId}`)
-      .then((response) => {
-        const { profile_description, profile_url, ...userData } = response.data;
-        setDescription(profile_description);
-        setAvatarUrl(profile_url);
-        setUser(userData);
-      })
-      .catch((error) => console.error(error));
-  }, [userId]);
-
-  if (!user) {
-    return <div>Loading...</div>;
-  }
+  const {user, description, setDescription, avatarUrl, setAvatarUrl, handleSubmit } = useMyProfile();
 
   const handleAvatarChange = (event) => {
     setAvatarUrl(event.target.value);
@@ -30,25 +11,7 @@ function MyProfile() {
 
   const handleDescriptionChange = (event) => {
     setDescription(event.target.value);
-  };
-
-  const handleSubmit = () => {
-    const updatedUser = {
-        description: description,
-        avatarUrl: avatarUrl,
-      };
-    
-      axios
-        .patch(`/api/users/${userId}`, updatedUser)
-        .then((response) => {
-          console.log("User updated successfully");
-          // Perform any necessary state updates or actions
-        })
-        .catch((error) => {
-          console.error("Error updating user:", error);
-          // Handle the error or display an error message
-        });
-  };
+  };  
 
   return (
     <Box display="flex" alignItems="center" justifyContent="center">
@@ -56,14 +19,18 @@ function MyProfile() {
         <Avatar
         sx={{ width: 200, height: 200, marginRight: 20 }}
         alt="User Avatar"
-        src={avatarUrl}
+        src={avatarUrl|| user.profile_url}
       />
       </Box>
       <Box>
+      {user && (
+          <>
         <Typography variant="h6" sx={{ mb: 1, mt: 2 }}>User: {user.username}</Typography>
         <Typography variant="body1" sx={{ mb: 1 }}>Email: {user.email}</Typography>
         <Typography variant="body1" sx={{ mb: 1 }}>First Name: {user.first_name}</Typography>
         <Typography variant="body1" sx={{ mb: 3 }}>Last Name: {user.last_name}</Typography>
+        </>
+        )}
         <TextField sx={{ mb: 3 }}
           label="Avatar URL"
           variant="outlined"
@@ -80,7 +47,7 @@ function MyProfile() {
           onChange={handleDescriptionChange}
           fullWidth
         />
-        <Button variant="contained" onClick={handleSubmit}>
+        <Button variant="contained" onClick={() => handleSubmit(description, avatarUrl)}>
           Submit
         </Button>
       </Box>
