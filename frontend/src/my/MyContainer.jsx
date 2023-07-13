ad16da26d100d2f7032a0ab7701e71f40bee7241
Yuze Wang <yuzewang621@gmail.com>
2023-07-12 20:30:30 -0600

    add link to profile, my movies, reviews and followers

--- frontend/src/my/MyContainer.jsx
+++ frontend/src/my/MyContainer.jsx
@@ -1,9 +1,48 @@
 import {Outlet} from "react-router";
+import { Link } from "react-router-dom";
 
 function MyContainer(props) {
-    return <div>
-        <Outlet />
-    </div>
+    return (
+        <div>
+          <nav style={{ display: "flex", justifyContent: "space-between" }}>
+            <ul style={{ display: "flex", listStyle: "none", padding: 0 }}> 
+              <li style={{
+              marginRight: "10px",
+              backgroundColor: "lightgrey",
+              borderRadius: "5px",
+              padding: "5px 10px",
+            }}>
+                <Link to="/my/profile">My Profile</Link>
+              </li>
+              <li style={{
+              marginRight: "10px",
+              backgroundColor: "lightgrey",
+              borderRadius: "5px",
+              padding: "5px 10px",
+            }}>
+                <Link to="/my/movies">My Movies</Link>
+              </li>
+              <li style={{
+              marginRight: "10px",
+              backgroundColor: "lightgrey",
+              borderRadius: "5px",
+              padding: "5px 10px",
+            }}>
+                <Link to="/my/reviews">My Reviews</Link>
+              </li>
+              <li style={{
+              marginRight: "10px",
+              backgroundColor: "lightgrey",
+              borderRadius: "5px",
+              padding: "5px 10px",
+            }}>
+                <Link to="/my/follows">My Follows</Link>
+              </li>
+            </ul>
+          </nav>
+          <Outlet />
+        </div>
+      );
 }
 
 export default MyContainer;
