2d6459a5baf18e94d49318b35c4034497a74777b
Yuze Wang <yuzewang621@gmail.com>
2023-07-12 19:34:03 -0600

    add new file for mymovies

--- frontend/src/my/MyMovies.jsx
+++ frontend/src/my/MyMovies.jsx
@@ -1,5 +1,58 @@
-function MyMovies() {
+import { Box, ThemeProvider, Button } from "@mui/material";
+import useMyMovies from "./useMyMovies";
+import MovieThumbnail from "../components/MovieThumbnail";
+import { IMAGE_BASE_URL } from "../utils/myApiBuilder";
+import theme from "../theme/mainTheme";
 
+
+function MyMovies() {
+  const { state, deleteMovie } = useMyMovies();
+  const handleDelete = (userId, movieId) => {
+    deleteMovie(userId, movieId);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box>
+        <Box
+          sx={{
+            mt: 2,
+            display: "flex",
+            flexWrap: "wrap",
+            gap: 2,
+            overflow: "auto",
+            flexGrow: 1,
+            flexShrink: 0,
+          }}
+        >
+          {state.movies.map((m) => (
+            <Box key={m.id} sx={{minWidth: 120, maxWidth: 240}}>
+            <MovieThumbnail 
+              key={m.id}
+              movie={{
+                id: m.id,
+                movie_id: m.movie_id,
+                name: m.title,
+                description: m.overview,
+                releaseDate: m.release_date,
+                poster: IMAGE_BASE_URL + m.poster_path,
+                rating: m.vote_average,
+              }}
+              sx={{ width: "100%" }}
+            />
+            <Button
+                variant="outlined"
+                onClick={() => handleDelete(1, m.movie_id)}
+                sx={{ marginTop: 2 }}
+              >
+                Remove
+              </Button>
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
 }
 
 export default MyMovies;
---
+++ frontend/src/my/useMyMovies.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useMyMovies = () => {
+  const [state, setState] = useState({
+    movies: [],
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    const fetchMyMovies = async () => {
+      try {
+        const response = await axios.get("/api/watchlists"); 
+        setState({
+          movies: response.data,
+          loading: false,
+          error: null,
+        });
+      } catch (error) {
+        setState({
+          movies: [],
+          loading: false,
+          error: error.message,
+        });
+      }
+      console.log(state.movies);
+    };
+
+    fetchMyMovies();
+  }, []);
+
+  const deleteMovie = async (userId, movieId) => {
+    const confirmed = window.confirm("Are you sure you want to remove this movie from your watchlist?");
+    if (confirmed) {
+    try {
+      console.log("state.movies: ", state.movies);
+      await axios.delete(`/api/watchlists/${userId}/${movieId}`);
+      // Optionally, you can update the state after successful deletion
+      const updatedMovies = state.movies.filter((movie) => movie.movie_id !== movieId);
+      console.log("updatedMovies", updatedMovies);
+      setState({
+        ...state,
+        movies: updatedMovies,
+      });
+    } catch (error) {
+      setState({
+        movies: [],
+        loading: false,
+        error: error.message,
+      });
+    }
+   }
+  };
+
+  return { state, deleteMovie };
+};
+
+export default useMyMovies;
