4863ef84a4e33c76327ee088315ce91c8175a94a
Yuze Wang <yuzewang621@gmail.com>
2023-07-12 20:31:17 -0600

    change function to get all the movies by userid and movie_id

--- backend/src/routes/watchlists.js
+++ backend/src/routes/watchlists.js
@@ -5,11 +5,35 @@ const router = require("express").Router();
 module.exports = db => {
   router.get("/watchlists", (request, response) =>{
     db.query(`
+    SELECT *
+    FROM movies
+    WHERE movie_id IN 
+      (
       SELECT movie_id
       FROM watchlists
+      WHERE user_id = 1
+      )
     `).then(({rows: watchlists }) => {
       response.json(watchlists);
     });
+  });
+
+  // DELETE route for deleting a movie from watchlists
+  router.delete("/watchlists/:userId/:movieId", (request, response) => {
+    const userId = request.params.userId;
+    const movieId = request.params.movieId;
+    console.log("movieId = ", movieId);
+    
+    db.query("DELETE FROM watchlists WHERE user_id = $1 AND  movie_id= $2", [userId, movieId])
+      .then(() => {
+        console.log("delete movieId", movieId, "by userId:", userId);
+        console.log("delete successfully!");
+        response.sendStatus(200); // Sending a success status code if the deletion is successful
+      })
+      .catch((error) => {
+        console.error(error);
+        response.sendStatus(500); // Sending an error status code if an error occurs during deletion
+      });
   });
 
   return router;
