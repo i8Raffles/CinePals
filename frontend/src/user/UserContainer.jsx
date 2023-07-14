8ef0bc618f1c7ae07c5435a9bc9febdeb6c84cf4
Yuze Wang <yuzewang621@gmail.com>
2023-07-14 12:34:17 -0600

    add link to each user page

--- frontend/src/user/UserContainer.jsx
+++ frontend/src/user/UserContainer.jsx
@@ -1,9 +1,48 @@
 import {Outlet} from "react-router";
+import { styled } from "@mui/system";
+import { NavLink } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+const StyledNavLink = styled(NavLink)(({ theme }) => ({
+    textDecoration: "none",
+    color: "black",
+    backgroundColor: "rgba(0, 0, 0, 0.08)",
+    borderRadius: "10px",
+    padding: "5px 10px",
+    marginRight: "10px",
+    fontSize: "18px",
+    "&:hover": {
+      backgroundColor: "lightgray",
+    },
+    "&.active": {
+      backgroundColor: "rgb(25, 118, 210)",
+      color: "white"
+    },
+  }));
 
 function UserContainer(props) {
-    return <div>
-        <Outlet />
-    </div>
+    const { userId } = useParams();
+    return (
+        <div>
+          <nav style={{ display: "flex", justifyContent: "space-between" }}>
+            <ul style={{ display: "flex", listStyle: "none", padding: 0 }}> 
+              <li>
+              <StyledNavLink to={`/user/${userId}/profile`} activeclassname="active">User Profile</StyledNavLink>
+              </li>
+              <li>
+              <StyledNavLink to={`/user/${userId}/movies`} activeclassname="active">User Movies</StyledNavLink>
+              </li>
+              <li>
+              <StyledNavLink to={`/user/${userId}/reviews`} activeclassname="active">User Reviews</StyledNavLink>
+              </li>
+              <li>
+              <StyledNavLink to={`/user/${userId}/follows`} activeclassname="active">User Follows</StyledNavLink>
+              </li>
+            </ul>
+          </nav>
+          <Outlet />
+        </div>
+      );
 }
 
 export default UserContainer;
