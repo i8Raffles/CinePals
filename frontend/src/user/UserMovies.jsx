dad808072d6245e9c145341c332b82ff40417e8b
Yuze Wang <yuzewang621@gmail.com>
2023-07-14 14:19:59 -0600

    add user movies page

--- frontend/src/user/UserMovies.jsx
+++ frontend/src/user/UserMovies.jsx
@@ -1,5 +1,52 @@
+import { Box, ThemeProvider } from "@mui/material";
+import useUserMovies from "./userUserMovies";
+import MovieThumbnail from "../components/MovieThumbnail";
+import { IMAGE_BASE_URL } from "../utils/myApiBuilder";
+import theme from "../theme/mainTheme";
+import { useParams } from "react-router-dom";
+import { format } from 'date-fns';
+
+
 function UserMovies(props) {
-    return <div>User movies</div>
+    const { userId } = useParams();
+    const { state } = useUserMovies(userId);
+
+
+    return (
+    <ThemeProvider theme={theme}>
+      <Box>
+        <Box
+          sx={{
+            mt: 2,
+            display: "flex",
+            flexWrap: "wrap",
+            gap: 2,
+            overflow: "auto",
+            flexGrow: 1,
+            flexShrink: 0,
+          }}
+        >
+          {state.movies.map((m) => (
+            <Box key={m.id} sx={{minWidth: 120, maxWidth: 240}}>
+            <MovieThumbnail 
+              key={m.id}
+              movie={{
+                id: m.id,
+                movie_id: m.movie_id,
+                name: m.title,
+                description: m.overview,
+                releaseDate: format(new Date(m.release_date), 'yyyy-MM-dd'),
+                poster: IMAGE_BASE_URL + m.poster_path,
+                rating: m.vote_average,
+              }}
+              sx={{ width: "100%" }}
+            />
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
 }
 
 export default UserMovies;
