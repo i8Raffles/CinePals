18e3e8067d6411518d45d6cf42675436ef88141f
Yuze Wang <yuzewang621@gmail.com>
2023-07-13 21:22:02 -0600

    add hook for myFollows page

---
+++ frontend/src/my/useMyFollows.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useMyFollows = () => {
+  const [follows, setFollows] = useState([]);
+  const userId = 1; //hard coding
+
+  const fetchFollows = async () => {
+    try {
+      const response = await axios.get(`/api/follows/${userId}`);
+      setFollows(response.data);
+    } catch (error) {
+      console.log("Error fetching follows:", error);
+    }
+  };
+
+  const handleUnfollow = async (userId, followId) => {
+    try {
+    console.log("userId :", userId, "Unfollow button clicked for follow ID:", followId);
+    await axios.patch(`/api/follows/${userId}/${followId}`);
+    fetchFollows();
+    } catch (error) {
+      console.log("Error unfollowing user:", error);
+    }
+  };
+
+
+  useEffect(() => {
+    fetchFollows();
+  }, []);
+
+  return { follows, handleUnfollow };
+};
+
+export default useMyFollows;
